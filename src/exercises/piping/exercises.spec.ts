5c6e5f7f9029115fd20d7a377ec9a44247eb3ed1
fstelzig <fabian.stelzig@andrena.de>
2022-03-29 17:15:20 +0200

    Add Piping exercises for take, first, of, from

--- src/exercises/piping/exercises.spec.ts
+++ src/exercises/piping/exercises.spec.ts
@@ -1,7 +1,59 @@
-import { of } from 'rxjs';
-import { albert, berta, charlotte } from '../chaining/users';
+import { first, from, of, take } from 'rxjs';
+import { albert } from '../chaining/users';
 import { cold } from 'jest-marbles';
+import { getCurrentUser } from '../chaining/chainingDemoApp';
 
 describe('creation', () => {
 
+    const numbers: number[] = [1, 2, 3, 5, 8, 13]
+
+    it('create an observable with single emit', () => {
+
+        // ↓ Your code here
+        const observable$ = of(numbers);
+        // ↑ Your code here
+
+        expect(observable$).toBeObservable(cold('(a|)', {
+            a: numbers,
+        }));
+
+        // Create an observable that emits numbers as a single value. Keep in mind, that the observable should only emit once.
+    });
+
+    it('create an observable with multiple emits', () => {
+
+        // ↓ Your code here
+        const observable$ = from(numbers);
+        // ↑ Your code here
+
+        expect(observable$).toBeObservable(cold('(abcdef|)', {
+            a: 1, b: 2, c: 3, d: 5, e: 8, f: 13
+        }))
+
+        // Create an observable that emits the content of the numbers array. Each value should be in the timeline.
+    });
+});
+
+describe('operators', () => {
+    it('take', () => {
+
+        const observable$ = getCurrentUser().pipe(
+            take(1)
+        );
+
+        expect(observable$).toBeObservable(cold('(0|)', {
+            0: null
+        }))
+    });
+
+    it('take first non null', () => {
+
+        const observable$ = getCurrentUser().pipe(
+            first(user => !!user)
+        );
+
+        expect(observable$).toBeObservable(cold('---(a|)', {
+            a: albert
+        }))
+    });
 });
