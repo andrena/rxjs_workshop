005e52fdd8212611720fd67aadfa9d6841666f3f
fstelzig <fabian.stelzig@andrena.de>
2022-05-17 06:12:51 +0200

    Add marbles code

--- src/demo/marblesDemo.spec.ts
+++ src/demo/marblesDemo.spec.ts
@@ -1,7 +1,7 @@
 // noinspection ES6UnusedImports
 
 import { cold, hot } from 'jest-marbles';
-import { first, map, merge, tap } from 'rxjs';
+import { catchError, filter, first, map, merge, min, of, tap } from 'rxjs';
 
 type Dog = {
     name: string,
@@ -42,23 +42,51 @@ const dogError: Error = {
 describe('example tests', () => {
 
     it('to be observable', () => {
+        const observable = cold('--a---b--c---d|', {a: fay, b: calli, c: prida, d: max});
 
-    });
+        let filtered = observable.pipe(
+            filter(dog => dog.age > 4)
+        )
 
-    it('hot', () => {
-
+        expect(filtered).toBeObservable(cold('---------c---d|', {a: fay, b: calli, c: prida, d: max}))
     });
 
     it('flush', () => {
 
+        let feedDog = jest.fn();
+        let observable = cold('--a---b--c---d|', {a: fay, b: calli, c: prida, d: max});
+
+
+        let feed = observable.pipe(
+            tap(dog => feedDog(dog))
+        )
+
+        let sub = observable.subscribe()
+
+        expect(feed).toSatisfyOnFlush(() => {
+            expect(feedDog).toHaveBeenCalledWith(fay)
+        })
     });
 
     it('Error case',() => {
+        let observable = cold('--a---b--#', {a: fay, b: calli, c: prida, d: max});
+
+        let minAge = observable.pipe(
+            map(dog => dog.age),
+            min(),
+            catchError((_) => of(dogError))
+        )
+
+        expect(minAge).toBeObservable(cold('---------(a|)', {a: dogError}))
 
     });
 
     it('emits at the same time', () => {
 
+        // ---a--b--
+        // ---d-----
+        // ---(ad)--
+        // ------x--
     });
 });
 
